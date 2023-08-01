87177cc0673662c3e09fac04b1cc5824b9681968
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-08-01 11:03:59 -0500

    5 done

--- c-sharp/CodeChallenge5/LinkedList.cs
+++ c-sharp/CodeChallenge5/LinkedList.cs
@@ -1,13 +1,63 @@
-namespace CodeChallenges 
+using System.Text;
+
+namespace CodeChallenges
 {
-    public class LinkedList
+  public class LinkedList
+  {
+    public Node Head { get; set; }
+
+    public LinkedList()
     {
-        // public Node Head {get; set;}
+      Head = null;
 
     }
 
-    public class Node
+    public void Insert(int value)
     {
-
+      Node newNode = new Node(value);
+      newNode.Next = Head;
+      Head = newNode;
     }
+
+    public bool Includes(int value)
+    {
+      Node current = Head;
+      while (current != null)
+      {
+        if (current.Value == value)
+          return true;
+        current = current.Next;
+      }
+      return false;
+    }
+
+    public override string ToString()
+    {
+      if (Head == null)
+        return "NULL";
+
+      Node current = Head;
+      StringBuilder result = new StringBuilder();
+      while (current != null)
+      {
+        result.Append($"{{ {current.Value} }} -> ");
+        current = current.Next;
+      }
+      result.Append("NULL");
+      return result.ToString();
+    }
+
+  }
+
+  public class Node
+  {
+    public int Value { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int value)
+    {
+      Value = value;
+      Next = null;
+    }
+  }
 }
