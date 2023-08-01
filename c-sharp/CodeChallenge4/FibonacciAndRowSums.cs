1df30964a07a0f5f38ae0ea75dabafa1684b7ae2
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-08-01 10:37:44 -0500

    4 done

--- c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -1,19 +1,49 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge4
+  public class CodeChallenge4
+  {
+    public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
     {
-        public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
-        {
-            int result = 0;
+      if (nthNumberInSequence <= 0)
+        return 0;
 
-            return result;
-        }
+      if (nthNumberInSequence == 1 || nthNumberInSequence == 2)
+        return 1;
 
-        public static int[] RowSums(int[][] matrix)
-        {
-            int[] rowSums = new int[matrix[0].Length];
-            return rowSums;
-        }
+      int prev = 1;
+      int current = 1;
 
+      for (int i = 3; i <= nthNumberInSequence; i++)
+      {
+        int next = prev + current;
+        prev = current;
+        current = next;
+      }
+
+      return current;
     }
+
+
+    public static int[] RowSums(int[][] matrix)
+    {
+      if (matrix.Length == 0)
+        return new int[0];
+
+      int rows = matrix.Length;
+      int cols = matrix[0].Length;
+      int[] rowSums = new int[rows];
+
+      for (int i = 0; i < rows; i++)
+      {
+        int sum = 0;
+        for (int j = 0; j < cols; j++)
+        {
+          sum += matrix[i][j];
+        }
+        rowSums[i] = sum;
+      }
+
+      return rowSums;
+    }
+  }
 }
