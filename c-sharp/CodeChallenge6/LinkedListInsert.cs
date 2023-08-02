217cd416aaa595c7ef7670b5faf6ab6a040a4eb3
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-08-02 13:13:13 -0500

    7 done

--- c-sharp/CodeChallenge6/LinkedListInsert.cs
+++ c-sharp/CodeChallenge6/LinkedListInsert.cs
@@ -1,66 +1,9 @@
 using System.Text;
+using CodeChallenges;
 
 namespace CodeChallenges
 {
-  public class Node
-  {
-    public int Value { get; set; }
-    public Node Next { get; set; }
-
-    public Node(int value)
-    {
-      Value = value;
-      Next = null;
-    }
-  }
-
-  public class LinkedList
-  {
-    public Node Head { get; set; }
-    public Node Tail { get; set; }
-
-    public LinkedList()
-    {
-      Head = null;
-      Tail = null;
-    }
-
-    public void Insert(int value)
-    {
-      Node newNode = new Node(value);
-      newNode.Next = Head;
-      Head = newNode;
-    }
-
-    public bool Includes(int value)
-    {
-      Node current = Head;
-      while (current != null)
-      {
-        if (current.Value == value)
-          return true;
-        current = current.Next;
-      }
-      return false;
-    }
-
-    public override string ToString()
-    {
-      if (Head == null)
-        return "NULL";
-
-      Node current = Head;
-      StringBuilder result = new StringBuilder();
-      while (current != null)
-      {
-        result.Append($"{{ {current.Value} }} -> ");
-        current = current.Next;
-      }
-      result.Append("NULL");
-      return result.ToString();
-    }
-  }
-
+  // Reference CodeChallenge5
   public class InsertableLinkedList : LinkedList
   {
     // Adds a new node with the given value to the end of the list
--- c-sharp/CodeChallenge7/LinkedListKth.cs
+++ c-sharp/CodeChallenge7/LinkedListKth.cs
@@ -1,10 +1,38 @@
-namespace CodeChallenges 
+using System;
+
+namespace CodeChallenges
 {
-    public class LinkedListKth
+  public class LinkedListKth : InsertableLinkedList
+  {
+    public int KthFromEnd(int kthFromEnd)
     {
-        public void KthFromEnd()
+      if (kthFromEnd < 0)
+      {
+        throw new ArgumentException("Must be positive.");
+      }
+
+      Node fast = base.Head; // Use the 'base' keyword to access the Head property
+      Node slow = base.Head; // Use the 'base' keyword to access the Head property
+
+      // Move 'fast' k nodes ahead of 'slow'
+      for (int i = 0; i < kthFromEnd; i++)
+      {
+        if (fast == null)
         {
-            
+          // The linked list doesn't have enough nodes
+          throw new ArgumentException("The linked list doesn't have enough nodes.");
         }
+        fast = fast.Next;
+      }
+
+      // Move both 'fast' and 'slow' until 'fast' reaches the end
+      while (fast.Next != null)
+      {
+        fast = fast.Next;
+        slow = slow.Next;
+      }
+
+      return slow.Value;
     }
+  }
 }
